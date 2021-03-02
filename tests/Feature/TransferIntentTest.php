b032a068ff3054dea31736dce76ba093e32fa20a
Diego Calle <dnetix@gmail.com>
2021-03-02 17:08:41 -0500

    Add tests for the query method

--- tests/Feature/TransferIntentTest.php
+++ tests/Feature/TransferIntentTest.php
@@ -42,4 +42,58 @@ class TransferIntentTest extends BaseTestCase
             'confirmationUrl' => 'https://dnetix.co/ping/callback',
         ]);
     }
+
+    /**
+     * @test
+     */
+    public function it_handles_an_approved_query_response()
+    {
+        $result = $this->service()->query('_SQC5uKmF6L');
+
+        $this->assertTrue($result->isApproved());
+        $this->assertFalse($result->isPending());
+        $this->assertFalse($result->isRejected());
+        $this->assertEquals('approved', $result->state());
+        $this->assertEquals('TRjJvCHT8qNj', $result->authorization());
+        $this->assertEquals('2021-03-02T14:57:27-05:00', $result->date());
+        $this->assertEquals('1614714969', $result->reference());
+        $this->assertEquals(3458, $result->amount());
+        $this->assertNull($result->description());
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_a_pending_query_response()
+    {
+        $result = $this->service()->query('_SQC5uKPEND');
+
+        $this->assertFalse($result->isApproved());
+        $this->assertTrue($result->isPending());
+        $this->assertFalse($result->isRejected());
+        $this->assertEquals('pending', $result->state());
+        $this->assertNull($result->authorization());
+        $this->assertNull($result->date());
+        $this->assertEquals('1614721251', $result->reference());
+        $this->assertEquals(3458, $result->amount());
+        $this->assertNull($result->description());
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_a_rejected_query_response()
+    {
+        $result = $this->service()->query('_SQC5uKREJE');
+
+        $this->assertFalse($result->isApproved());
+        $this->assertFalse($result->isPending());
+        $this->assertTrue($result->isRejected());
+        $this->assertEquals('pending', $result->state());
+        $this->assertNull($result->authorization());
+        $this->assertNull($result->date());
+        $this->assertEquals('1614721251', $result->reference());
+        $this->assertEquals(3458, $result->amount());
+        $this->assertEquals('Expired', $result->description());
+    }
 }
